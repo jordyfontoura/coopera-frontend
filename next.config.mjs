a63e8c63b99b9319b79a4b21a7f84a58770690c2
Jordy Fontoura <jordyfontoura@outlook.com>
2024-03-23 12:19:30 -0300

    feat: add sass compiler

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
