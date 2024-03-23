12f710c296023588092cbefc681c308fb683b061
Jordy Fontoura <jordyfontoura@outlook.com>
2024-03-23 12:35:01 -0300

    fix: replace __dirname with './'

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import path from 'path';
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join('./', 'styles')],
   },
 };
 
