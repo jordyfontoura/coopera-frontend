41e342839e7577c3e8f92c004cb33c88188491f4
Caroline Yumi <carolineyumin@gmail.com>
2024-04-05 17:05:34 -0300

    feat: adicionar estrutura inicial do footer

---
+++ components/layout/footer/footer.component.tsx
@@ -0,0 +1,13 @@
+import styles from './footer.module.scss';
+
+/**
+ * Footer component
+ * @return {JSX.Element}
+ */
+export function Footer(): JSX.Element {
+  return (
+    <footer className={styles.footer}>
+      Footer
+    </footer>
+  );
+}
