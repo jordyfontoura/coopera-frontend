550e7c08508c0904b2f3f2312c92fa54bdb6ee9b
Caroline Yumi <carolineyumin@gmail.com>
2024-04-10 17:51:45 -0300

    feat: adicionar footer

--- components/layout/footer/footer.component.tsx
+++ components/layout/footer/footer.component.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import styles from './footer.module.scss';
 
 /**
@@ -7,7 +9,65 @@ import styles from './footer.module.scss';
 export function Footer(): JSX.Element {
   return (
     <footer className={styles.footer}>
-      Footer
+      <div className={styles.container}>
+        <Link href="/">
+          <Image
+            src={'/icons/logo-variant2.svg'}
+            alt="Logo Coopera Esportes"
+            width={210}
+            height={77}
+          />
+        </Link>
+        <div className={styles.socials}>
+          <a href="https://www.instagram.com/cooperaesportes/">
+            <Image
+              src={'/icons/socials/instagram.svg'}
+              alt="Instagram"
+              width={32}
+              height={32}
+            />
+          </a>
+          <a href="https://www.facebook.com/CooperaEsportes/">
+            <Image
+              src={'/icons/socials/facebook.svg'}
+              alt="Facebook"
+              width={32}
+              height={32}
+            />
+          </a>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/sobre">Sobre Nós</Link>
+            </li>
+            <li>
+              <Link href="/contato">Contato</Link>
+            </li>
+            <li>
+              <Link href="/eventos">Eventos</Link>
+            </li>
+            <li>
+              <Link href="/mentores">Mentores</Link>
+            </li>
+            <li>
+              <Link href="/seja-bolsista">Seja um bolsista</Link>
+            </li>
+            <li>
+              <Link href="/seja-mentor">Seja um mentor</Link>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <Image
+            src={'/icons/GEFlogo.svg'}
+            alt="Logo GEF"
+            width={210}
+            height={57}
+          />
+        </div>
+      </div>
+      <p>&copy; Coopera Ltda. Todos os diretos reservados. </p>
     </footer>
   );
 }
