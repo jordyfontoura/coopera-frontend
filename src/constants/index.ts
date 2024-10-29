c20248d18cf8b7b75e17d20f816bd691a372e452
Jordy Fontoura <jordyfontoura@outlook.com>
2024-10-29 20:24:40 -0300

    Atualiza link de contato no menu de navegação

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,5 +4,5 @@ export const NavLinks = [
   { href: "/mentores", label: "Mentores" },
   { href: "/bolsistas", label: "Bolsistas" },
   { href: "/processo-seletivo", label: "Processo Seletivo" },
-  { href: "https://ig.me/m/cooperaesportes", label: "Contato" },
+  { href: "https://docs.google.com/forms/d/e/1FAIpQLSfJrMbkQOXDfMtff8gLH6RmNgB0btUvrldliE14Y5xauymb_w/viewform?usp=sf_link", label: "Contato" },
 ];
